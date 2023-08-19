7904da56b6f94cf4600654d0f7f62d957cd889c5
claustra01 <shinya430.g@gmail.com>
2023-08-19 18:04:39 +0900

    add: filebase routing sample

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1>This is TopPage</h1>
+      <p onClick={() => {window.location.href = '/example'}}>redirect to /example</p>
+    </>
+  )
+}
+
+export default App
