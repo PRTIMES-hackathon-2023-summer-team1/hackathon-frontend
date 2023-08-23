79af3796290a45b3522c82b8d5cac3c5085f0e90
claustra01 <shinya430.g@gmail.com>
2023-08-23 14:59:11 +0900

    add: dummy tour data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,56 @@
 function App() {
 
+  const dummyTours = [
+    {
+      "tour_lists": {
+        "tour_id": "string",
+        "name": "string",
+        "description": "string",
+        "price": 0,
+        "dates": {
+          "first_day": "string",
+          "last_day": "string"
+        },
+        "capacity_people": {
+          "max": 0,
+          "current": 0
+        }
+      }
+    },
+    {
+      "tour_lists": {
+        "tour_id": "string",
+        "name": "string",
+        "description": "string",
+        "price": 0,
+        "dates": {
+          "first_day": "string",
+          "last_day": "string"
+        },
+        "capacity_people": {
+          "max": 0,
+          "current": 0
+        }
+      }
+    },
+    {
+      "tour_lists": {
+        "tour_id": "string",
+        "name": "string",
+        "description": "string",
+        "price": 0,
+        "dates": {
+          "first_day": "string",
+          "last_day": "string"
+        },
+        "capacity_people": {
+          "max": 0,
+          "current": 0
+        }
+      }
+    }
+  ]
+
   return (
     <>
       <h1>This is TopPage</h1>
