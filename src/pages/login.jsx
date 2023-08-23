d4319bbc4566f18158c40d1ba4f368d9ce2b1004
risa-skmt <skmt.risa@gmail.com>
2023-08-23 19:11:02 +0900

    ログインCSS設定

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react"
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function App() {
@@ -32,12 +35,49 @@ function App() {
   return (
     <>
     <h3>Login</h3>
+
+
+
+  
     <form onSubmit={handleSubmit}>
-        <label>E-mail<input name="email" value={email} onChange={(e) => setEmail(e.target.value)}/ ></label>
-        <label>パスワード<input name="password" value={password} onChange={(e) => setPassWord(e.target.value)}/ ></label>
-        <button type="submit">ログイン</button>
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus/>
+            <TextField
+            value={password} 
+            onChange={(e) => setPassWord(e.target.value)}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"/>
+               </Box>
+        {/* <label>E-mail<input name="email" value={email} onChange={(e) => setEmail(e.target.value)}/ ></label> */}
+        {/* <label>パスワード<input name="password" value={password} onChange={(e) => setPassWord(e.target.value)}/ ></label> */}
+        <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Login
+            </Button>
+
     </form>
     
+   
     </>
   )
 }
