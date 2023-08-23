586115aa5e4d4aebb19a4afd96d049a5190d3b13
risa-skmt <skmt.risa@gmail.com>
2023-08-23 16:26:30 +0900

    topページに一覧表示/dummydata

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,60 +1,28 @@
+import { getTours } from "../../lib/getTours"
+import './index.css' 
+
 function App() {
 
-  const dummyTours = [
-    {
-      "tour_lists": {
-        "tour_id": "string",
-        "name": "string",
-        "description": "string",
-        "price": 0,
-        "dates": {
-          "first_day": "string",
-          "last_day": "string"
-        },
-        "capacity_people": {
-          "max": 0,
-          "current": 0
-        }
-      }
-    },
-    {
-      "tour_lists": {
-        "tour_id": "string",
-        "name": "string",
-        "description": "string",
-        "price": 0,
-        "dates": {
-          "first_day": "string",
-          "last_day": "string"
-        },
-        "capacity_people": {
-          "max": 0,
-          "current": 0
-        }
-      }
-    },
-    {
-      "tour_lists": {
-        "tour_id": "string",
-        "name": "string",
-        "description": "string",
-        "price": 0,
-        "dates": {
-          "first_day": "string",
-          "last_day": "string"
-        },
-        "capacity_people": {
-          "max": 0,
-          "current": 0
-        }
-      }
-    }
-  ]
+  
 
   return (
     <>
       <h1>This is TopPage</h1>
       <p onClick={() => {window.location.href = '/example'}}>redirect to /example</p>
+    
+      <ul className="container">  
+        {getTours().map(tour => 
+          <a href={`/tours/${tour.tour_lists.tour_id}`}>  
+            <li className="tourInfo">
+            <h2>{tour.tour_lists.name}</h2> 
+            <p>{tour.tour_lists.description}</p>
+            <h4>{tour.tour_lists.price}</h4>
+            </li>
+          </a>
+        )}
+       
+      </ul>
+      
     </>
   )
 }
