56a458235145a4ce3204b975b243aeceae9a0bb5
risa-skmt <skmt.risa@gmail.com>
2023-08-24 16:21:13 +0900

    booking遷移変更

--- src/pages/tours/[id].jsx
+++ src/pages/tours/[id].jsx
@@ -1,83 +1,84 @@
 import { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom"
-import axios from "axios"
-import { Box, Button, Divider } from '@mui/material'
-import markdownit from 'markdown-it'
-import dayjs from 'dayjs'
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Box, Button, Divider } from "@mui/material";
+import markdownit from "markdown-it";
+import dayjs from "dayjs";
 import Loading from "../../components/loading";
 
-
 export default function ViewOneTour() {
-
-  const tourId = useParams().id
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(true)
-  const [isLoggedIn, setIsLoggedIn] = useState(false) // 仮置き，認証ができたらContextに移行
-  const [tourData, setTourData] = useState({})
+  const tourId = useParams().id;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 仮置き，認証ができたらContextに移行
+  const [tourData, setTourData] = useState({});
 
   // ツアー情報を取得
   const getTourData = async () => {
     try {
       const response = await axios.get(`/tours/${tourId}`, {
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `DummyToken`,
+          "Content-Type": "application/json",
+          Authorization: `DummyToken`,
         },
-      })
+      });
       if (response.status === 200) {
-        setTourData(response.data)
-        setIsLoading(false)
+        setTourData(response.data);
+        setIsLoading(false);
       } else {
-        console.error(error)
-        navigate('/')
+        console.error(error);
+        navigate("/");
       }
     } catch (error) {
-      console.error(error)
-      navigate('/')
+      console.error(error);
+      navigate("/");
     }
-  }
+  };
 
   const redirectToBooking = () => {
     if (isLoggedIn) {
-      navigate(`/tours/booking/${tourId}`)
+      navigate(`/tours/booking/${tourId}`);
     } else {
-      navigate(`/login?redirect=/tours/booking/${tourId}`)
+      navigate(`/login?redirect=/tours/booking/${tourId}`);
     }
-  }
+  };
 
   useEffect(() => {
-    getTourData()
-  }, [])
+    getTourData();
+  }, []);
 
   return (
     <div className="detail">
-      {isLoading ? <Loading /> : (
+      {isLoading ? (
+        <Loading />
+      ) : (
         <>
           <div className="Info">
             <h2>{tourData.name}</h2>
             <p>{tourData.description}</p>
-            <p>{dayjs(tourData.first_day).format("YYYY/MM/DD hh:mm")} ~ {dayjs(tourData.last_day).format("YYYY/MM/DD hh:mm")}</p>
+            <p>
+              {dayjs(tourData.first_day).format("YYYY/MM/DD hh:mm")} ~{" "}
+              {dayjs(tourData.last_day).format("YYYY/MM/DD hh:mm")}
+            </p>
             <p>{tourData.price}円/人</p>
           </div>
 
-          <Box sx={{ m: '1ch' }}>
+          <Box sx={{ m: "1ch" }}>
             <Divider variant="inset" />
-            <div style={{ textAlign: 'left' }}>
+            <div style={{ textAlign: "left" }}>
               <div
-                dangerouslySetInnerHTML={{ __html: markdownit().render(String(tourData.body)) }}
+                dangerouslySetInnerHTML={{
+                  __html: markdownit().render(String(tourData.body)),
+                }}
               />
             </div>
           </Box>
 
-          <Button
-            variant="contained"
-            onClick={redirectToBooking}
-          >
+          <Button variant="contained" onClick={redirectToBooking}>
             このツアーを予約する
           </Button>
-
         </>
       )}
     </div>
-  )
+  );
 }
