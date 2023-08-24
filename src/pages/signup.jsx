b241b5c525cbeeb0e18ea49eebaa5c6ff0e79170
claustra01 <shinya430.g@gmail.com>
2023-08-24 23:09:54 +0900

    add: create new user

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import axios from "axios"
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
@@ -10,39 +11,25 @@ function App() {
     const [email, setEmail] = useState('');
     const [password, setPassWord] = useState('');
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch('/signup', {
-                method: "POST", // *GET, POST, PUT, DELETE, etc.
-                
-                headers: {
-                  "Content-Type": "application/json",
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                
-                body: JSON.stringify({
-                'name': name,
-                'email': email,
-                'password': password}), // 本体のデータ型は "Content-Type" ヘッダーと一致させる必要があります
-              });
-              return response.json(); // JSON のレスポンスをネイティブの JavaScript オブジェクトに解釈
-            }
-            
-        catch (error) {
-            console.error(error);
-          }
-        }   
+    const handleSubmit = async () => {
+      try {
+        const response = await axios.post('/users/signup', {
+          name: name,
+          email: email,
+          password: password,
+        })
+        console.log(response)
+      } catch (error) {
+        console.error(error)
+      }
+    }
 
   return (
     <>
 
-
     <h3>サインアップ</h3>
     
-    <form onSubmit={handleSubmit}>
-    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+    <Box component="form" noValidate sx={{ mt: 1 }}>
     <TextField
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -76,19 +63,17 @@ function App() {
               type="password"
               id="password"
               autoComplete="current-password"/>
-               </Box>
 
         <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              SignUp
-            </Button>
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          onClick={handleSubmit}
+        >
+          SignUp
+        </Button>
+      </Box>
 
-    </form>
-    
     </>
   )
 }
