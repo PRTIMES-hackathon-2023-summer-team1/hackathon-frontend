a4c2dfb1b2d6b644d2f814297ddf949026e3b57c
risa-skmt <skmt.risa@gmail.com>
2023-08-25 11:51:12 +0900

    booking form css

--- src/pages/tours/booking/[tour_id].jsx
+++ src/pages/tours/booking/[tour_id].jsx
@@ -1,19 +1,20 @@
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
 import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import Loading from "../../../components/loading";
-import "./tour_id.css";
+import {
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Typography,
+} from "@mui/material";
 
 function App() {
-
   const { tour_id } = useParams();
   const navigate = useNavigate();
   const token = sessionStorage.getItem("token");
@@ -57,14 +58,14 @@ function App() {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-      })
+      });
       const response = await api.post(`/bookings`, {
         tour_id: tour_id,
         participants: people,
-      })
+      });
       if (response.status === 200) {
         // 予約一覧ページへリダイレクト
-        navigate('/bookings')
+        navigate("/bookings");
       } else {
         console.error(response.error);
       }
@@ -73,45 +74,66 @@ function App() {
     }
   };
 
-  if (isLoading) return <Loading/>;
+  if (isLoading) return <Loading />;
 
   return (
-    <>
-      <div className="bookingForm">
-        <Paper elevation={3} sx={{ width: 600 }} className="booking">
-          <p>Tour:{tourData.name}</p>
-          {/* <p>your name: {}</p> */}
-          <p>user_id:{tourData.user_id}</p>
-          <p>price: {tourData.price * people + "円"}</p>
-          <p>
-            date: {dayjs(tourData.first_day).format("YYYY/MM/DD hh:mm")} -
-            {dayjs(tourData.last_day).format("YYYY/MM/DD hh:mm")}
-          </p>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">People</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={people}
-                label="People"
-                onChange={handleChange}
-                sx={{ width: 200 }}
-              >
-                {Array.from({
-                  length: tourData.max_capacity - tourData.current_capacity,
-                }).map((data, index) => (
-                  <MenuItem value={index + 1}>{index + 1}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <Button variant="contained" onClick={postBookingInfo}>
-              予約
-            </Button>
-          </Box>
-        </Paper>
-      </div>
-    </>
+    <div className="detail">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="tour-details">
+          {/* 以前のコード */}
+
+          <Paper elevation={3} className="booking-form" sx={{ my: 2 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                padding: 4,
+              }}
+            >
+              <Typography variant="h5">Tour: {tourData.name}</Typography>
+              <Typography>User ID: {tourData.user_id}</Typography>
+              <Typography>Price: {tourData.price * people}円</Typography>
+              <Typography>
+                Date: {dayjs(tourData.first_day).format("YYYY/MM/DD hh:mm")} -{" "}
+                {dayjs(tourData.last_day).format("YYYY/MM/DD hh:mm")}
+              </Typography>
+              <FormControl sx={{ minWidth: 120, marginTop: 3 }}>
+                <InputLabel id="people-select-label">People</InputLabel>
+                <Select
+                  labelId="people-select-label"
+                  id="people-select"
+                  value={people}
+                  label="People"
+                  onChange={handleChange}
+                  sx={{ width: 200 }}
+                >
+                  {Array.from({
+                    length: tourData.max_capacity - tourData.current_capacity,
+                  }).map((data, index) => (
+                    <MenuItem key={index} value={index + 1}>
+                      {index + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </Paper>
+
+          <Button
+            className="booking-button"
+            variant="contained"
+            onClick={postBookingInfo}
+            size="large"
+            sx={{ width: "100%", padding: 1, margin: 2 }}
+          >
+            予約
+          </Button>
+        </div>
+      )}
+    </div>
   );
 }
 export default App;
