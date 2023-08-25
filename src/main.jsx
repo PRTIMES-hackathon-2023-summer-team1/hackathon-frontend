2f0d0c8df4476d75a33e2fac3bdad752c9d70232
risa-skmt <skmt.risa@gmail.com>
2023-08-25 09:59:19 +0900

    css

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import axios from 'axios'
-import './index.css'
-import { Routes } from 'generouted/react-router'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
+import { Routes } from "generouted/react-router";
 
-axios.defaults.baseURL = 'http://localhost:8080/'
+axios.defaults.baseURL = "http://localhost:8080/";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Routes />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
